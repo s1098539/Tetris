f93e798c57928b5b8ab202ffc4b93bd1b2161414
lion <s1098539@student.hsleiden.nl>
2018-04-08 20:50:16 +0200

    Menu logout now gives a toast message

--- app/src/main/java/com/example/lion/tetris/activity/MenuActivity.java
+++ app/src/main/java/com/example/lion/tetris/activity/MenuActivity.java
@@ -3,10 +3,13 @@ package com.example.lion.tetris.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.lion.tetris.R;
 import com.example.lion.tetris.activity.game.GameActivity;
@@ -66,6 +69,7 @@ public class MenuActivity extends AppCompatActivity {
                 if (auth.getCurrentUser() != null) {
                     auth.signOut();
                     logout.setText(R.string.menu_login);
+                    Toast.makeText(getApplication(), R.string.menu_toast_logout, Toast.LENGTH_LONG).show();
                 } else {
                     Intent intent = new Intent(MenuActivity.this, LoginActivity.class);
                     startActivity(intent);
